c8c7a736844f04ceb35564ccc62ec1e93c225f63
Sahil-at-Work <sahil.asawant30@gmail.com>
2025-05-05 09:48:05 +0530

    Update post1.ts

--- src/data/posts/post1.ts
+++ src/data/posts/post1.ts
@@ -2,7 +2,7 @@ import { Post } from '../../types';
 
 const post: Post = {
   id: '1',
-  title: 'Understanding Pythagorean Theorem',
+  title: 'Gravitation',
   images: [
     {
       url: 'https://github.com/user-attachments/assets/effabc04-9440-4b42-a6e6-43560936b9a6',
@@ -13,19 +13,19 @@ const post: Post = {
       caption: ''
     },
     {
-      url: 'https://github.com/user-attachments/assets/bf44ecfc-c5c0-4682-a044-4cfd114bc1a5',
+      url: 'https://github.com/user-attachments/assets/b1e7394a-e202-4e00-a6cc-e159d61fc9b4',
       caption: ''
     },
     {
-      url: 'https://github.com/user-attachments/assets/bf44ecfc-c5c0-4682-a044-4cfd114bc1a5',
+      url: 'https://github.com/user-attachments/assets/420ece48-45e8-4ed7-ac96-76c379bcba85',
       caption: ''
     },
     {
-      url: 'https://github.com/user-attachments/assets/bf44ecfc-c5c0-4682-a044-4cfd114bc1a5',
+      url: 'https://github.com/user-attachments/assets/1fc9cd57-1a44-48de-b4c1-3e37cdc03994',
       caption: ''
     },
     {
-      url: 'https://github.com/user-attachments/assets/bf44ecfc-c5c0-4682-a044-4cfd114bc1a5',
+      url: 'https://github.com/user-attachments/assets/ccbfd5b0-7a0c-4a69-baaf-8b25318b39b8',
       caption: ''
     }
   ],
