c93194fa02a9ac5331f147eeeb8fa7eb9a1faf4e
Sahil-at-Work <sahil.asawant30@gmail.com>
2025-05-25 20:06:48 +0530

    ms

--- src/data/quiz/index.ts
+++ src/data/quiz/index.ts
@@ -2,13 +2,16 @@ import mathQuiz from './math-quiz';
 import physicsQuiz from './physics-quiz';
 import chemistryQuiz from './chemistry-quiz';
 import biologyQuiz from './biology-quiz';
+import vectorsQuiz from './vectors-quiz';
+
 import { Quiz } from '../../types';
 
 export const quizzes: Quiz[] = [
   mathQuiz,
   physicsQuiz,
   chemistryQuiz,
-  biologyQuiz
+  biologyQuiz,
+  vectorsQuiz
 ];
 
 export const getQuizById = (id: string): Quiz | undefined => {
